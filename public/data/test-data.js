3d3f606a325e1d532c8adf807ebc60d00e4d75be
Julia Cymerman <j.cymerman@medidok.de>
2023-11-08 22:09:41 +0100

    Add data to test-data.js

--- public/data/test-data.js
+++ public/data/test-data.js
@@ -1,45 +1,151 @@
 export default {
-    title: "Test wiedzy ogólnej",
+    title: "Test wiedzy o piłce nożnej",
     questions: [{
             timeSpent: 0,
-            question: "Jakie miasto jest stolicą Polski?",
-            correctAnswer: "Warszawa",
+            question: "Kto ma najwięcej złotych butów?",
+            correctAnswer: "Lionel Messi",
             answers: [{
-                    content: "Warszawa",
+                    content: "Lionel Messi",
                     id: "first"
                 },
                 {
-                    content: "Kraków",
+                    content: "Cristiano Ronaldo",
                     id: "second"
                 },
                 {
-                    content: "Gniezno",
+                    content: "Marc Andre Ter Stegen",
                     id: "third"
                 },
                 {
-                    content: "Lubin",
+                    content: "Luis Suarez",
                     id: "fourth"
                 }]
         },
         {
             timeSpent: 0,
-            question: "Co nie jest dzielnicą Lublina?",
-            correctAnswer: "Pruszków",
+            question: "Kto ma najwięcej złotych piłek?",
+            correctAnswer: "Lionel Messi",
             answers: [{
-                    content: "Pruszków",
+                    content: "Cristiano Ronaldo",
                     id: "first"
                 },
                 {
-                    content: "Wrotków",
+                    content: "Lionel Messi",
                     id: "second"
                 },
                 {
-                    content: "Śródmieście",
+                    content: "Michael Plattini",
                     id: "third"
                 },
                 {
-                    content: "Tatary",
+                    content: "Fabio Cannavaro",
                     id: "fourth"
                 }]
-        }]
+        },
+        {
+            timeSpent: 0,
+            question: "Kto wygrał najwięcej razy mundial?",
+            correctAnswer: "Brazylia",
+            answers: [{
+                    content: "Argentyna",
+                    id: "first"
+                },
+                {
+                    content: "Polska",
+                    id: "second"
+                },
+                {
+                    content: "Niemcy",
+                    id: "third"
+                },
+                {
+                    content: "Brazylia",
+                    id: "fourth"
+                }]
+        },
+        {
+            timeSpent: 0,
+            question: "Kto wygrał pierwszy mundial?",
+            correctAnswer: "Urugwaj",
+            answers: [{
+                    content: "Argentyna",
+                    id: "first"
+                },
+                {
+                    content: "Niemcy",
+                    id: "second"
+                },
+                {
+                    content: "Urugwaj",
+                    id: "third"
+                },
+                {
+                    content: "Brazylia",
+                    id: "fourth"
+                }]
+        },
+        {
+            timeSpent: 0,
+            question: "Kto wygrał mundial w 2022 roku?",
+            correctAnswer: "Argentyna",
+            answers: [{
+                    content: "Francja",
+                    id: "first"
+                },
+                {
+                    content: "Rosja",
+                    id: "second"
+                },
+                {
+                    content: "Chorwacja",
+                    id: "third"
+                },
+                {
+                    content: "Argentyna",
+                    id: "fourth"
+                }]
+        },
+        {
+            timeSpent: 0,
+            question: "Kto strzelił najwięcej goli w ElClassico?",
+            correctAnswer: "Lionel Messi",
+            answers: [{
+                    content: "Cristiano Ronaldo",
+                    id: "first"
+                },
+                {
+                    content: "Neymar Jr",
+                    id: "second"
+                },
+                {
+                    content: "Robert Lewandowski",
+                    id: "third"
+                },
+                {
+                    content: "Lionel Messi",
+                    id: "fourth"
+                }]
+        },
+        {
+            timeSpent: 0,
+            question: "Kto wygrał kobiecą ligę mistrzyń w sezonie 2022/2023?",
+            correctAnswer: "FC Barcelona",
+            answers: [{
+                    content: "Real Madryt",
+                    id: "first"
+                },
+                {
+                    content: "PSG",
+                    id: "second"
+                },
+                {
+                    content: "VLF Wolfsburg",
+                    id: "third"
+                },
+                {
+                    content: "FC Barcelona",
+                    id: "fourth"
+                }]
+        }
+    ]
 };
